9e7723372184ee474d54811f0e94c26608269499
Srinath Suresh Kumar <srinathsk18@gmail.com>
2026-03-15 20:04:41 -0400

    Remove commented-out FenwickTree implementation
    Removed commented-out implementation of FenwickTree class and main function.

--- fenwick.cpp
+++ fenwick.cpp
@@ -3,54 +3,6 @@
 
 using namespace std;
 
-// class FenwickTree {
-//     std::vector<int> tree;
-
-// public:
-//     // Initialize tree with 0s. Size is n + 1 because BIT is 1-indexed.
-//     FenwickTree(int n) : tree(n + 1, 0) {}
-
-//     // Adds 'val' to the element at index 'i' (1-indexed)
-//     void update(int i, int val) {
-//         for (; i < tree.size(); i += i & -i) {
-//             tree[i] += val;
-//         }
-//     }
-
-//     // Returns the prefix sum from 1 to 'i'
-//     int query(int i) {
-//         int sum = 0;
-//         for (; i > 0; i -= i & -i) {
-//             sum += tree[i];
-//         }
-//         return sum;
-//     }
-
-//     // Returns the sum of the range [l, r]
-//     int queryRange(int l, int r) {
-//         return query(r) - query(l - 1);
-//     }
-// };
-
-// int main() {
-//     std::vector<int> data = {1, 3, 5, 7, 9, 11};
-//     FenwickTree ft(data.size());
-
-//     // Build the tree
-//     for (int i = 0; i < data.size(); i++) {
-//         ft.update(i + 1, data[i]);
-//     }
-
-//     std::cout << "Sum of first 3 elements: " << ft.query(3) << std::endl; // 1+3+5 = 9
-//     std::cout << "Sum of range [2, 5]: " << ft.queryRange(2, 5) << std::endl; // 3+5+7+9 = 24
-
-//     // Real-time update: Change element at index 3 (value 5) by adding 2
-//     ft.update(3, 2); 
-//     std::cout << "New sum of first 3 elements: " << ft.query(3) << std::endl; // 9 + 2 = 11
-
-//     return 0;
-// }
-
 
 class BIT {
 public:
